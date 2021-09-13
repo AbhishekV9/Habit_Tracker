2827a0be995806a3994c716ac36ebec0b82e7d65
Abhishek Verma <avishekv444@gmail.com>
2021-09-13 19:47:06 +0530

    express is up

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express=require('express');
 const app=express();
-const port=8000;
+const port=8800;
+
+const db=require('./config/mongoose');
 
 app.set('view engine','ejs');
 app.set('views','./views');
+
+app.use(express.urlencoded);
+
+app.use('/',require('./routes'));
 
 app.listen(port,function(err){
     if(err){
