d3c8f0b44512dac3a89c8761db13cfa660e83372
Abhishek Verma <avishekv444@gmail.com>
2021-09-15 01:18:54 +0530

    update controller in week controller

--- controller/weekController.js
+++ controller/weekController.js
@@ -12,3 +12,41 @@ module.exports.weekView=function(req,res){
         });
     })
 }
+
+module.exports.update= async function(req,res){
+  try{
+        id=req.params.id;
+        day=req.params.day;
+        value=req.params.value;
+        const habit=await habits.findById(id);
+        habit.Days[day]=value;
+        await habits.findByIdAndUpdate(id,{Days:habit.Days});
+        let noOfCompletedDays=0;
+        let currentStrek=0;
+        let streak=0;
+        for(let i=0;i<habit.Days.length;i++){
+            
+            if(habit.Days[i]=='Completed'){
+                noOfCompletedDays++;
+                currentStrek++;
+            }else{
+                if(currentStrek>streak){
+                    streak=currentStrek;
+                    currentStrek=0;
+                }
+            }
+        }
+        if(currentStrek>streak){
+            streak=currentStrek;
+        }
+        await habits.findByIdAndUpdate(id,{
+            Streak:streak,
+            Completed:noOfCompletedDays
+        })
+        res.redirect('back');
+
+  }catch(err){
+
+  }
+  
+}
--- routes/week.js
+++ routes/week.js
@@ -4,5 +4,6 @@ const router=express.Router();
 const weekController=require('../controller/weekController');
 
 router.get('/',weekController.weekView);
+router.get('/update/:id/:day/:value',weekController.update);
 
 module.exports=router;
