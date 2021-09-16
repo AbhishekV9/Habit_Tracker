2c59756dd48aba6b5925d9427b70bd8ae875bc38
Abhishek Verma <avishekv444@gmail.com>
2021-09-16 16:49:43 +0530

    given style to weekly page

---
+++ assets/js/week.js
@@ -0,0 +1,11 @@
+let change = document.getElementsByClassName('dropdown-toggle');
+
+
+for(let i of change){
+    if(i.innerHTML.includes('Completed') ){
+        i.classList.add("bg-success");
+    }
+    else if(i.innerHTML.includes('Incomplete')){
+        i.classList.add("bg-danger");
+    }
+}
