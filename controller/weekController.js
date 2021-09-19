7db2e2444f91f85814070691b85c3c06ee8a0831
Abhishek Verma <avishekv444@gmail.com>
2021-09-20 01:34:52 +0530

    deployed

--- controller/weekController.js
+++ controller/weekController.js
@@ -10,7 +10,7 @@ module.exports.weekView = function (req, res) {
   for (let i = 0; i < 7; i++) {
     let date =
     today.getDate() + '-' + Month[today.getMonth()] + ' ' + today.getFullYear();
-      today.setDate(today.getDate() - 1); 
+    today.setDate(today.getDate() - 1); 
     days.push(date);
   }
   //rereese array
